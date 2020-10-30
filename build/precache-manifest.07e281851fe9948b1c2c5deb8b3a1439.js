d2317c9f52fb0bfaa3cfb43fdb977dd2cff9e0d3
Pankaj Dhami <pandhami1@publicisgroupe.net>
2020-10-30 16:45:53 +0530

    added build folder

---
+++ build/precache-manifest.07e281851fe9948b1c2c5deb8b3a1439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98d32d2aada768f9eb308db6526b4a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d6c19aa737818c29219",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "665fb07efa2f63fd41a6",
+    "url": "/static/js/2.fe47d933.chunk.js"
+  },
+  {
+    "revision": "8d6c19aa737818c29219",
+    "url": "/static/js/main.adffdb49.chunk.js"
+  },
+  {
+    "revision": "1893be08da971e1fc9d5",
+    "url": "/static/js/runtime-main.913038c8.js"
+  }
+]);
